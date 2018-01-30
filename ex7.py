21f29b64b8a6b86b5366b5b2f46582fc264e1be4
Rob Hutton <rhutton31179@outlook.com>
2018-01-29 20:55:41 -0600

    More messing with ex7

--- ex7.py
+++ ex7.py
@@ -19,3 +19,11 @@ end12 = "r"
 # watch end = '' at the end.  try removing it to see what happens
 print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)
+# By default the print function adds a newline as the last character
+# Which if written out would be end = '\n'
+# If we want something other than a new line we need to specify it
+# In this case we are specifying a space
+# Which gets you the output Cheese Burger
+# Instead of
+# Cheese
+# Burger
